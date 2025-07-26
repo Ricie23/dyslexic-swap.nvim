b7b64514290049c7cc6a66c323218202543d2834
RIch Robertson <rrobertson@Mac.lan>
2025-07-26 14:35:52 -0400

    change insert keymapping

--- lua/dyslexic-letter-swap.lua
+++ lua/dyslexic-letter-swap.lua
@@ -26,7 +26,7 @@ function M.setup()
   vim.keymap.set("n", "z,", swap_letters, { desc = "Swap letters to the left" })
 
   -- Insert mode mapping: <C-x>
-  vim.keymap.set("i", "<C-g>", function()
+  vim.keymap.set("i", "<C-,>", function()
     -- Save current mode position
     local row, col = unpack(vim.api.nvim_win_get_cursor(0))
 
